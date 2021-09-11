af9d963aa11e0d802fee4b7b7633edd5c37e7a1c
Soni Mohandas <sonireetha@gmail.com>
2021-09-11 18:41:55 -0400

    file edited

--- static/js/app.js
+++ static/js/app.js
@@ -11,32 +11,35 @@ function init(){
           .text(sample)
           .property("value", sample);
       });
+    //   Select the first value sampleNames array
+      let newSample = sampleNames[0];
+      demographicInfo(newSample);
+      samplePlots(newSample);
+
   });
 };
-  
+
+function optionChanged(newSample){
+    demographicInfo(newSample);
+    samplePlots(newSample);
+}
+ 
 // Initialize the selection
 init();
 
-// Create function optionChanged called in html
-function optionChanged(newSample) {
-    // console.log(newSample);
-    demographicInfo(newSample);
-    samplePlots(newSample);
-};
-
 // Create function demographicInfo called in function optionChanged
-function demographicInfo(sample){
+function demographicInfo(nextSample){
     d3.json("data/samples.json").then((data) => {
-      let demoInfo = data.metadata.filter(sampleObj => sampleObj.id == sample);
-      //   console.log(demoInfo);
+      let demoInfo = data.metadata.filter(newObj => newObj.id == nextSample);
+    //   console.log(demoInfo);
       //demoInfo returned like an array
       let result = demoInfo[0];
-      //   Create a panel using html id sample-metadata 
+    //   Create a panel using html id sample-metadata 
       let panel = d3.select("#sample-metadata");      
-      //Clear panel for the next selection
+      //Clear panel for each selection
       panel.html("");
-      // Select next option and append to the panel
-      Object.entries(result).forEach(([key, value]) => {
+    // Append metadata keys and values for each selection
+    Object.entries(result).forEach(([key, value]) => {
         panel.append("p").html(`<strong>${key}: </strong> ${value}`)
     });
     });
